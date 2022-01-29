3c39450dcb8916fd87724969e3b6effb94b05f0c
JesusNagao <98624459+JesusNagao@users.noreply.github.com>
2022-01-29 00:18:53 -0600

    Add files via upload

---
+++ Fabry Perot (simulacion)/FabryPerot2.m
@@ -0,0 +1,24 @@
+clear all;
+clc;
+
+r=0.8; %Reflection coefficient
+d=7.5e-6; %Distance between glass plates
+epsilon=[0:1e-2:pi]; %Incidence angle
+lambda=5e-6; %wavelength
+w0=50 %Beam Waist
+
+%[t,eps]=meshgrid(elevation,epsilon);
+
+%Important relations
+Delta=2*d*cos(epsilon);
+delta=2*pi*Delta/lambda;
+[d1,d2]=meshgrid(delta,delta);
+
+F=(4*r^2)/((1-r^2)^2);
+a=1; %Amplitude of the incident wave
+
+I=a^2./(1+F.*(sin((d1.^2+d2.^2)./2).^2)) %Intensity of a Fabry-Perot interferometer
+
+
+h=surf(I)
+set(h,'LineStyle','none')
